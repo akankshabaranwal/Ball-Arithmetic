6c9629953c6925af51010b5ab297848b57fe77ce
Gyorgy Rethy <grethy@student.ethz.ch>
2021-03-20 19:53:50 +0100

    [benchmark] Add the tsc header

--- src/arb-test.c
+++ src/arb-test.c
@@ -1,14 +1,25 @@
-#include <stdio.h>
-
-#include "arb.h"
+#include <arb.h>
+#include <tsc_x86.h>
 
 int main()
 {
-    arb_t x;
-    arb_init(x);
-    arb_const_pi(x, 50 * 3.33);
-    arb_printn(x, 50, 0); flint_printf("\n");
-    flint_printf("Computed with arb-%s\n", arb_version);
-    arb_clear(x);
+    slong prec;
+    arb_t x, y;
+    arb_init(x); arb_init(y);
+    myInt64 start = start_tsc();
+    for (prec = 64; ; prec *= 2) {
+        arb_const_pi(x, prec);
+        arb_set_si(y, -10000);
+        arb_exp(y, y, prec);
+        arb_add(x, x, y, prec);
+        arb_sin(y, x, prec);
+        if (arb_rel_accuracy_bits(y) >= 53) break;
+    }
+    myInt64 cycles = stop_tsc(start);
+    printf("Ran for %llu cycles\n", cycles);
+
+    arb_printn(y, 15, 0); printf("\n");
+    arb_clear(x); arb_clear(y);
+    flint_cleanup();
     return 0;
 }
