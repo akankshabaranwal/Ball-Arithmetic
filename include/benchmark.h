0f98a2bca7cc40f688fc2be74f5a1326f827fe75
Kouroche Bouchiat <kbouchiat@student.ethz.ch>
2021-05-26 09:28:02 +0200

    Add multi-precision to benchmark

--- include/benchmark.h
+++ include/benchmark.h
@@ -13,13 +13,24 @@ typedef struct benchmark_struct_t
     benchmark_fun_t deinit;
     char *name;
     double divisor;
+    unsigned int prec_start;
+    unsigned int prec_stop;
 } benchmark_t;
 
 #define BENCHMARK_BEGIN_TABLE() benchmark_t _TABLE[] = {
 #define BENCHMARK_END_TABLE()   { 0 }};
-#define BENCHMARK_FUNCTION(_function, _init, _deinit, _divisor)\
-    { .name = #_function, .function = _function, .init = _init, .deinit = _deinit, .divisor = _divisor },
+#define BENCHMARK_FUNCTION(_function, _init, _deinit, _divisor, _prec_start, _prec_stop) \
+    {                               \
+        .name = #_function,         \
+        .function = _function,      \
+        .init = _init,              \
+        .deinit = _deinit,          \
+        .divisor = _divisor,        \
+        .prec_start = _prec_start,  \
+        .prec_stop = _prec_stop     \
+    },
 
-#define BENCHMARK_FOREACH(_iterator) for (benchmark_t *_iterator = _TABLE; _iterator->function != NULL; _iterator++)
+#define BENCHMARK_FOREACH(_iterator) \
+    for (benchmark_t *_iterator = _TABLE; _iterator->function != NULL; _iterator++)
 
 #endif // !BALL_ARITHMETIC_BENCHMARK_H
