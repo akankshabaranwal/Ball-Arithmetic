f0b134a40009381ec0b1b8bd9746f57d22d7d845
abaranwal <abaranwal@student.ethz.ch>
2021-05-30 14:24:43 +0200

    tested some corner cases for apfp add

--- src/unittest.c
+++ src/unittest.c
@@ -235,20 +235,20 @@ TEST_GROUP(apfp, {
         ASSERT_EQUAL_UL(apfp_test[0]->mant->limbs[0], 0x15BF0995AAF790ull);
     });
 
-    TEST_CASE(apfp_add addition with positive numbers, {
+    TEST_CASE(apfp_add addition with positive numbers 1.000...00 X 2^10 + 1.000...00 X 2^10, {
             // Check mantissa first
             // Then check expected exponent
-            apfp_set_mant(apfp_test[0], 0, 0);
-            apfp_set_mant(apfp_test[0], 1, 0);
-            apfp_set_mant(apfp_test[0], 2, 0);
             apfp_set_mant(apfp_test[0], 3, 9223372036854775808llu);
+            apfp_set_mant(apfp_test[0], 2, 0);
+            apfp_set_mant(apfp_test[0], 1, 0);
+            apfp_set_mant(apfp_test[0], 0, 0);
             apfp_test[0]->mant->sign = 1;
             apfp_test[0]->exp = 10;
 
-            apfp_set_mant(apfp_test[1], 0, 0);
-            apfp_set_mant(apfp_test[1], 1, 0);
-            apfp_set_mant(apfp_test[1], 2, 0);
             apfp_set_mant(apfp_test[1], 3, 9223372036854775808llu);
+            apfp_set_mant(apfp_test[1], 2, 0);
+            apfp_set_mant(apfp_test[1], 1, 0);
+            apfp_set_mant(apfp_test[1], 0, 0);
             apfp_test[1]->mant->sign = 1;
             apfp_test[1]->exp = 10;
 
@@ -257,11 +257,71 @@ TEST_GROUP(apfp, {
             //Check value of mantissa
             ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 3), 9223372036854775808llu);
             ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 2), 0llu);
-            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 2), 0llu);
-            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 2), 0llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 1), 0llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 0), 0llu);
 
             //Check value of exponent and sign
             ASSERT_EQUAL_L(apfp_test[2]->exp, 11lu);
+            ASSERT_EQUAL_I(apfp_test[2]->mant->sign, 1);
+    });
+
+    TEST_CASE(apfp_add addition with positive numbers 1.111...11 X 2^10 + 1.111...11 X 2^10, {
+            // Check mantissa first
+            // Then check expected exponent
+            apfp_set_mant(apfp_test[0], 3, 18446744073709551615llu);
+            apfp_set_mant(apfp_test[0], 2, 18446744073709551615llu);
+            apfp_set_mant(apfp_test[0], 1, 18446744073709551615llu);
+            apfp_set_mant(apfp_test[0], 0, 18446744073709551615llu);
+            apfp_test[0]->mant->sign = 1;
+            apfp_test[0]->exp = 10;
+
+            apfp_set_mant(apfp_test[1], 3, 18446744073709551615llu);
+            apfp_set_mant(apfp_test[1], 2, 18446744073709551615llu);
+            apfp_set_mant(apfp_test[1], 1, 18446744073709551615llu);
+            apfp_set_mant(apfp_test[1], 0, 18446744073709551615llu);
+            apfp_test[1]->mant->sign = 1;
+            apfp_test[1]->exp = 10;
+
+            apfp_add(apfp_test[2], apfp_test[0], apfp_test[1]);
+
+            //Check value of mantissa
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 3), 18446744073709551615llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 2), 18446744073709551615llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 1), 18446744073709551615llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 0), 18446744073709551615llu);
+
+            //Check value of exponent and sign
+            ASSERT_EQUAL_L(apfp_test[2]->exp, 11lu);
+            ASSERT_EQUAL_I(apfp_test[2]->mant->sign, 1);
+    });
+
+    TEST_CASE(apfp_add addition with positive numbers 1.000...01 X 2^256 + 1.000...00 X 2^1, {
+            // Check mantissa first
+            // Then check expected exponent
+            apfp_set_mant(apfp_test[0], 3, 9223372036854775808llu);
+            apfp_set_mant(apfp_test[0], 2, 0);
+            apfp_set_mant(apfp_test[0], 1, 0);
+            apfp_set_mant(apfp_test[0], 0, 1);
+            apfp_test[0]->mant->sign = 1;
+            apfp_test[0]->exp = 256;
+
+            apfp_set_mant(apfp_test[1], 3, 9223372036854775808llu);
+            apfp_set_mant(apfp_test[1], 2, 0);
+            apfp_set_mant(apfp_test[1], 1, 0);
+            apfp_set_mant(apfp_test[1], 0, 0);
+            apfp_test[1]->mant->sign = 1;
+            apfp_test[1]->exp = 1;
+
+            apfp_add(apfp_test[2], apfp_test[0], apfp_test[1]);
+
+            //Check value of mantissa
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 3), 9223372036854775808llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 2), 0llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 1), 0llu);
+            ASSERT_EQUAL_UL(apint_getlimb(apfp_test[2]->mant, 0), 2llu);
+
+            //Check value of exponent and sign
+            ASSERT_EQUAL_L(apfp_test[2]->exp, 256lu);
             ASSERT_EQUAL_I(apfp_test[2]->mant->sign, 1);
     });
 
